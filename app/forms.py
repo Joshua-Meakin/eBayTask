6fb2973549b46e402214164c0c1fca1d81cbd780
SH-Xu <240495108@qq.com>
2021-07-10 10:57:01 +0100

    update

--- app/forms.py
+++ app/forms.py
@@ -8,7 +8,7 @@ class SubmitForm(FlaskForm):
     d_postcode= TextField("Delivery postcode",[validators.Required("Please enter your delivery postcode.")])
     vehicle = SelectField('Vehicle', choices = [('Bicycle', 'Bicycle'), 
                                                 ('Motorbike', 'Motorbike'), 
-                                                ('Parcel car', 'Parcal car'),
+                                                ('Parcel car', 'Parcel car'),
                                                 ('Small van', 'Small van'),
                                                 ('Large van', 'Large van')])
     submit = SubmitField("Calculate the price")
